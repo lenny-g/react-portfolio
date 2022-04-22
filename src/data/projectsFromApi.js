7fceed6578563fd29ec7aa7b1bdf13bf9643d30e
lenny-g <leanne_gallag@live.co.uk>
2022-04-22 12:30:32 +0100

    added external links

--- src/data/projectsFromApi.js
+++ src/data/projectsFromApi.js
@@ -119,8 +119,7 @@ export const projectsFromApi = [
   },
   {
     id: "13",
-    projectImg:
-      "https://awesomescreenshot.s3.amazonaws.com/image/2487608/26050408-d09889970f174d1a92c5741ced5feb1d.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20220421%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220421T153432Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=b577d8ae32c1e27afa90bb71fab4b0db486c6fd1642979e61dfe27f8cccb30c5",
+    projectImg: "https://i.ibb.co/tqLHFVG/nosql-social-network.png",
     title: "NoSQL Social Network",
     description:
       "The objective of this project was to create an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. Using Express.js for routing, MongoDB and Mongoose ODM.",
@@ -129,8 +128,7 @@ export const projectsFromApi = [
   },
   {
     id: "14",
-    projectImg:
-      "https://awesomescreenshot.s3.amazonaws.com/image/2487608/26052836-923b091e935659511a65102d039ecea7.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20220421%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220421T162133Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=cf77e615f6b3b7ef00f1f20f3aa9f506058ded262e829cffb6d44215a2e94fec",
+    projectImg: "https://i.ibb.co/DfkqVV5/mern-book-search.png",
     title: "MERN Book Search Engine",
     description:
       "The objective of this project was to take starter code with a fully functioning Google Books API search engine built with a RESTful API, and refactor it to be a GraphQL API built with Apollo Server. The app was built using the MERN stack with a React front end, MongoDB database, and Node.js/Express.js server and API. It's already set up to allow users to save book searches to the back end.",
@@ -139,8 +137,7 @@ export const projectsFromApi = [
   },
   {
     id: "15",
-    projectImg:
-      "https://awesomescreenshot.s3.amazonaws.com/image/2487608/26051155-fe611550186fbf2f759b63503a7d7e44.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20220421%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220421T154727Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=0deaa556eaef9690ec39185da7944e0ae9b25567a33eb7e7753342d3fa92471c",
+    projectImg: "https://i.ibb.co/fCpKyY0/pwa-text-editor.png",
     title: "PWA Text Editor",
     description:
       "The objective of this project was to build a text editor that runs in the browser. The app will be a single-page application that meets the PWA criteria. Additionally, it will feature a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. The application will also function offline.",
@@ -149,8 +146,7 @@ export const projectsFromApi = [
   },
   {
     id: "16",
-    projectImg:
-      "https://awesomescreenshot.s3.amazonaws.com/image/2487608/26050358-b94452f2948ddf1a5ede189304400aaa.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20220421%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220421T153327Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=fe3ad537c5f8e28a8343cc0c6ce10eb8e73a82757bcc28575256b1efb938fb1f",
+    projectImg: "https://i.ibb.co/zJXB85K/schoolify-screenshot.png",
     title: "Schoolify",
     description:
       "Schoolify is an app created to close the gap between school and home. With Schoolify teachers are able to instantly message parents about students behaviour, give out awards and have all their students on one dashboard. Parents are able to update medical information, add children and absence requests which can be approved or declined in real time.",
